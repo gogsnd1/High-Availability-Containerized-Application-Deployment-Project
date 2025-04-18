1ebd641640a1cf8f7759e408ce1398a896a11f76
karlohs <150075905+karlohs01@users.noreply.github.com>
2025-04-17 22:50:10 -0400

    updated DB to adapt to the new app

--- saleor_app/eCommerceDB.sql
+++ saleor_app/eCommerceDB.sql
@@ -1,19 +1,20 @@
-/* table for keeping track of users who visit ecommerce site */
-CREATE TABLE User {
-    userID PRIMARY KEY NOT NULL;
-    userName VARCHAR NOT NULL;
-    userAddress VARCHAR NOT NULL;
+/* table to store the questions */
+CREATE TABLE Question {
+    questionID SERIAL PRIMARY KEY NOT NULL;
+    title VARCHAR(100) NOT NULL;
+    question TEXT NOT NULL;
+    difficultyID INT REFERENCES difficultyLevel(difficultyID);
 }
-/* table for orders that are made by the users */
-CREATE TABLE Order {
-    orderID INT FOREIGN KEY NOT NULL;
-    orderQuantity INT NOT NULL;
-    orderPrice FLOAT(10, 2) NOT NULL;
-    orderAddress VARCHAR NOT NULL;
+
+/* table to store the answers */
+CREATE TABLE Answer {
+    answerID SERIAL PRIMARY KEY NOT NULL;
+    questionID INT REFERENCES Question(questionID) ON DELETE CASCADE;
+    answer VARCHAR(255) NOT NULL;
 }
-/* table for payments made by the users for their order */
-CREATE TABLE Payment {
-    paymentID INT FOREIGN KEY;
-    paymentMethod TEXT;
-    paymentStatus BOOLEAN;
+
+/* table to hold difficulty levels and be able to reference questions with them */
+CREATE TABLE difficultyLevel {
+    difficultyID SERIAL PRIMARY KEY;
+    difficultyName VARCHAR(20) UNIQUE NOT NULL --easy, medium, hard
 }
