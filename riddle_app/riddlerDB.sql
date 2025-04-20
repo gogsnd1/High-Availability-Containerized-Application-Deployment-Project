d7bd5775c60784f262e231d4a77a4d32ee9097fc
Gil <gogsnd1@gmail.com>
2025-04-20 04:40:03 +0000

    Initialized swarm and made 3 frontend and 2 backend replicas which im happy to say are working

--- riddle_app/riddlerDB.sql
+++ riddle_app/riddlerDB.sql
@@ -1,20 +1,20 @@
+/* table to hole difficulty levels and be able to reference questions with them */
+CREATE TABLE IF NOT EXISTS difficultyLevel (
+	difficultyID SERIAL PRIMARY KEY NOT NULL,
+	difficultyName VARCHAR(20) UNIQUE NOT NULL --easy, medium, hard
+);
 /* table to store the questions */
-CREATE TABLE Question {
-    questionID SERIAL PRIMARY KEY NOT NULL;
-    title VARCHAR(100) NOT NULL;
-    question TEXT NOT NULL;
-    difficultyID INT REFERENCES difficultyLevel(difficultyID);
-}
+CREATE TABLE IF NOT EXISTS Question (
+	questionID SERIAL PRIMARY KEY NOT NULL,
+	title VARCHAR(100) NOT NULL,
+	question TEXT NOT NULL,
+	difficultyID INT REFERENCES difficultyLevel(difficultyID)
+);
 
 /* table to store the answers */
-CREATE TABLE Answer {
-    answerID SERIAL PRIMARY KEY NOT NULL;
-    questionID INT REFERENCES Question(questionID) ON DELETE CASCADE;
-    answer VARCHAR(255) NOT NULL;
-}
+CREATE TABLE IF NOT EXISTS Answer (
+    answerID SERIAL PRIMARY KEY NOT NULL,
+    questionID INT REFERENCES Question(questionID) ON DELETE CASCADE,
+    answer VARCHAR(255) NOT NULL
+);
 
-/* table to hold difficulty levels and be able to reference questions with them */
-CREATE TABLE difficultyLevel {
-    difficultyID SERIAL PRIMARY KEY;
-    difficultyName VARCHAR(20) UNIQUE NOT NULL --easy, medium, hard
-}
